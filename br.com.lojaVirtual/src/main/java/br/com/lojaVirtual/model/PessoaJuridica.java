ea0e6ab5bb213afa9499e3327ecde80ff6dd99fd
Marcus Aurélio M. O. Santos <90445199+marcusaurelio743@users.noreply.github.com>
2026-09-12 10:09:31 -0300

    criando classe pessoa_fisica e juridica

---
+++ br.com.lojaVirtual/src/main/java/br/com/lojaVirtual/model/PessoaJuridica.java
@@ -0,0 +1,65 @@
+package br.com.lojaVirtual.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pessoa_juridica")
+@PrimaryKeyJoinColumn(name = "id")
+public class PessoaJuridica extends Pessoa {
+	
+	private static final long serialVersionUID = 1L;
+	@Column(length = 150)
+	private String cnpj;
+	@Column(length = 150)
+	private String inscEstadual;
+	@Column(length = 150)
+	private String inscMunicipal;
+	@Column(length = 150)
+	private String nomeFantasia;
+	@Column(length = 150)
+	private String razaoSocial;
+	@Column(length = 150)
+	private String categoria;
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public String getInscEstadual() {
+		return inscEstadual;
+	}
+	public void setInscEstadual(String inscEstadual) {
+		this.inscEstadual = inscEstadual;
+	}
+	public String getInscMunicipal() {
+		return inscMunicipal;
+	}
+	public void setInscMunicipal(String inscMunicipal) {
+		this.inscMunicipal = inscMunicipal;
+	}
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+
+}
